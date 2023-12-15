ef0067c0f54246e96e02a66645761cfc767a12f7
unknown <moshiur.mirage@gmail.com>
2023-12-15 19:58:22 +0600

    facing css overloading

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyB7eYKvaPoAPgEenUN34zT5J4DVIfwQ93E",
+  authDomain: "animaetdcontest.firebaseapp.com",
+  projectId: "animaetdcontest",
+  storageBucket: "animaetdcontest.appspot.com",
+  messagingSenderId: "858890709086",
+  appId: "1:858890709086:web:eddb9d7833a534c6e4eccf"
 };
 
 
